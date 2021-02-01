bdf172649e399e2adfdec201be7b62623350b3e4
Filip <filip.knias@o2.pl>
2021-02-01 15:58:42 +0100

    React router and chakra ui setup

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+// React router
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// Pages
+import Dashboard from "../pages/Dashboard";
+import Account from "../pages/Account";
 
 const App = () => {
-  return <h1>Hello React</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/register" component={Account} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
